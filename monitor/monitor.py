13176049f748aa6d71c7ab2cf7865ef67aeb53d8
Jorge Conde <condejorgedev@gmail.com>
2015-07-10 15:02:36 -0700

    modified monitor

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,6 +1,7 @@
 volume = 0
 noise = 0
 duration = 0
+symptom = ""
 
 while 1:
 	f = open('../AlarmClock/data', 'r')
@@ -10,11 +11,28 @@ while 1:
 		if data != "":
 			split_data = data.split(" ")
 
-			if volume != split_data[0] or noise != split_data[1] or duration != split_data[2]:
-				volume = split_data[0]
-				noise = split_data[1]
-				duration = split_data[2]
+			if str(volume) != split_data[0] or str(noise) != split_data[1] or str(duration) != split_data[2]:
+				volume = int(split_data[0])
+				noise = int(split_data[1])
+				duration = int(split_data[2])
+
+				# Volume too low
+				if volume < noise:
+					symptom = "increase "
+					symptom += str(noise + 1)
+
+				# Volume too high
+				elif volume > (noise + 1):
+					symptom = "decrease "
+					symptom += str(volume - 1)
+
+				# Duration too high
+				elif duration > 15000:
+					symptom = "increase"
+					symptom += str(volume + 5)
+
 				print data
+				print symptom
 
 	except Exception, e:
 		raise e
